bdb8aa648934c73eaeb585cac2fc81c62e66c3cf
ANTOINE_NAME TUJAS_NAME <antoine.tujas33@gmail.com>
2023-06-01 10:41:17 +0200

    react 4 fait

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,7 @@
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard({pokemon}) {
 
+  console.log(pokemon);
+  
   return (
     <figure>
       {pokemon.imgSrc != undefined ? (
@@ -13,16 +14,5 @@ function PokemonCard() {
     </figure>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
